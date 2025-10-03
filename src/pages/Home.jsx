8b7fb91ab686bc18b1f5036ac135bc614d4411d9
Anjali-pandey11 <pandeyanjali7906@gmail.com>
2025-10-03 19:42:59 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import FeaturedDestination from '../components/FeaturedDestination'
+import ExclusiveOffer from '../components/ExclusiveOffer'
+import Testimonial from '../components/Testimonial'
+
+const Home = () => {
+  return (
+   <>
+   <Hero/>
+   <FeaturedDestination/>
+   <ExclusiveOffer/>
+   <Testimonial/>
+   </>
+  )
+}
+
+export default Home
